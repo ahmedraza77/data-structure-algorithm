91fbba7f9ee74bb3b849b691211b47a49320ae8e
Ahmed Raza <araza10@safeway.com>
2021-12-29 20:19:27 +0530

    changes to CommonItem problem

--- src/com/problems/CommonItems.java
+++ src/com/problems/CommonItems.java
@@ -1,24 +1,24 @@
 package com.problems;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CommonItems {
     public static void main(String[] args) {
-        int[] arr1 = {'a', 'x', 'f', 's'};
+        int[] arr1 = {'a', 'x', 'c', 's'};
         int[] arr2 = {'c', 'v', 'b'};
 
         System.out.println(findCommonItems(arr1, arr2));
     }
 
     private static Boolean findCommonItems(int[] firstArray, int[] secondArray) {
-        Map<Integer, Boolean> firstArrayValues = new HashMap<>();
+        Set<Integer> firstArrayValues = new HashSet<>();
         for (int element : firstArray) {
-            firstArrayValues.put(element, Boolean.TRUE);
+            firstArrayValues.add(element);
         }
 
         for (int element : secondArray) {
-            if(firstArrayValues.containsKey(element)){
+            if(firstArrayValues.contains(element)){
                 return true;
             }
         }
