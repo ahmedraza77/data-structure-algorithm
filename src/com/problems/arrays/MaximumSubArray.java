264f23f00da36ab6b300b01c82ee19b597a07753
Ahmed Raza <araza10@safeway.com>
2022-01-01 16:27:45 +0530

    commit maxSubArray problem

---
+++ src/com/problems/arrays/MaximumSubArray.java
@@ -0,0 +1,22 @@
+package com.problems.arrays;
+
+public class MaximumSubArray {
+    public static void main(String[] args) {
+
+        int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println("Maximum contiguous sum is "
+                + maxSubArraySum(a, a.length));
+    }
+
+    private static int maxSubArraySum(int[] a, int length) {
+        int max = a[0];
+        int curr_max = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            curr_max = (a[i] < (curr_max + a[i])) ? (curr_max + a[i]) : a[i];
+            max = (curr_max > max) ? curr_max : max;
+        }
+        return max;
+    }
+}
+
