6e4a808c879d1dc1136e78b25dc791cc0976f9c6
Ahmed Raza <araza10@safeway.com>
2022-01-05 22:04:02 +0530

    adding tree implementation

---
+++ src/com/implementation/tree/MyBinarySearchTree.java
@@ -0,0 +1,55 @@
+package com.implementation.tree;
+
+public class MyBinarySearchTree {
+    TreeNode root;
+    int size;
+
+    MyBinarySearchTree () {
+        this.root = null;
+        this.size = 3;
+    }
+
+    public void insert(int value) {}
+
+    public boolean lookup(int value) {
+        if(root==null){
+            return false;
+        }
+        TreeNode currentNode = root;
+        while(currentNode!=null) {
+            if(value > currentNode.value){
+                currentNode = currentNode.right;
+            }
+            else if(value < currentNode.value) {
+                currentNode = currentNode.left;
+            }
+            else if(value == currentNode.value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void remove() {}
+
+    public void printTree() {
+        System.out.println(root);
+    }
+
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+//        tree.insert(9);
+//        tree.insert(4);
+//        tree.insert(6);
+//        tree.insert(20);
+//        tree.insert(170);
+//        tree.insert(15);
+//        tree.insert(1);
+
+        System.out.println(tree.lookup(20));
+    }
+
+    //       9
+    //    4     20
+    //  1  6  15  170
+}
---
+++ src/com/implementation/tree/TreeNode.java
@@ -0,0 +1,22 @@
+package com.implementation.tree;
+
+public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "value=" + value +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
